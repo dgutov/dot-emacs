81f5d3d4eb0edd901868df16e54b4005b2b8585b
Dmitry Gutov <dgutov@yandex.ru>
2012-04-23 06:28:08 +0400

    Customize hippie-expand

---
+++ hippie.el
@@ -0,0 +1,44 @@
+(defvar he-undo-stack nil)
+
+(defun hippie-expand-undo ()
+  (interactive)
+  (setq this-command 'hippie-expand)
+  (if (eq last-command this-command)
+      (when he-undo-stack
+        (if (cdr he-undo-stack)
+            (undo)
+          (hippie-expand -1))
+        (let ((cell (pop he-undo-stack)))
+          (setq he-tried-table (car cell)
+                he-num (cdr cell))))
+    (setq he-undo-stack nil)))
+
+(defadvice hippie-expand (around expand-lisp-in-minibuffer activate)
+  (let ((hippie-expand-try-functions-list
+         (if (and (window-minibuffer-p (selected-window))
+                  (not (eq major-mode 'emacs-lisp-mode)))
+             (hippie-try-list-with-lisp)
+           hippie-expand-try-functions-list))
+        (hippie-expand-only-buffers (list major-mode)))
+    (unless (eq last-command this-command)
+      (setq he-undo-stack nil))
+    (when (or (not (integerp arg)) (plusp arg))
+      (push (cons he-tried-table he-num) he-undo-stack))
+    ad-do-it))
+
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev
+        try-expand-dabbrev-all-buffers
+        try-complete-file-name-partially
+        try-complete-file-name))
+
+(defun hippie-try-list-with-lisp ()
+  (append '(try-complete-lisp-symbol-partially
+            try-complete-lisp-symbol)
+          hippie-expand-try-functions-list))
+
+(add-lambda 'emacs-lisp-mode-hook
+  (set (make-local-variable 'hippie-expand-try-functions-list)
+       (hippie-try-list-with-lisp)))
+
+(provide 'hippie)
--- keys.el
+++ keys.el
@@ -6,6 +6,7 @@
 (require 'transpose-frame)
 (require 'winring)
 (require 'dired)
+(require 'hippie)
 
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
@@ -65,6 +66,7 @@
 (global-set-key (kbd "M-P") 'fastnav-sprint-backward)
 
 (global-set-key (kbd "C-/") 'auto-complete)
+(global-set-key (kbd "M-?") 'hippie-expand-undo)
 (global-set-key (kbd "C-c s") 'eproject-grep)
 (global-set-key (kbd "C-h j") 'javadoc-lookup)
 (global-set-key (kbd "C-c a") 'webjump-api)
