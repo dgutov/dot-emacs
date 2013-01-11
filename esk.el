291920fdf1f4ff5a50e73e1fc8dc24d665da0e9e
Dmitry Gutov <dgutov@yandex.ru>
2013-01-11 10:10:18 +0400

    Reindent esk-remove-ecl-on-save

--- esk.el
+++ esk.el
@@ -64,12 +64,12 @@
           1 font-lock-warning-face t))))
 
 (defun esk-remove-elc-on-save ()
-    "If you're saving an elisp file, likely the .elc is no longer valid."
-    (make-local-variable 'after-save-hook)
-    (add-hook 'after-save-hook
-              (lambda ()
-                (if (file-exists-p (concat buffer-file-name "c"))
-                    (delete-file (concat buffer-file-name "c"))))))
+  "If you're saving an elisp file, likely the .elc is no longer valid."
+  (make-local-variable 'after-save-hook)
+  (add-hook 'after-save-hook
+            (lambda ()
+              (if (file-exists-p (concat buffer-file-name "c"))
+                  (delete-file (concat buffer-file-name "c"))))))
 
 (add-hook 'prog-mode-hook 'esk-local-comment-auto-fill)
 (add-hook 'prog-mode-hook 'esk-turn-on-save-place-mode)
