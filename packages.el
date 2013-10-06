7995496bf56f5beb9b0324528f30aef176c1ab42
Dmitry Gutov <dgutov@yandex.ru>
2013-10-06 19:27:09 +0300

    Include bbdb

--- packages.el
+++ packages.el
@@ -10,7 +10,7 @@
     iflipb inf-ruby iy-go-to-char markdown-mode move-text multiple-cursors
     paredit rainbow-mode ruby-electric rinari haml-mode sass-mode smex
     switch-window typing undo-tree wgrep win-switch ruby-hash-syntax
-    yaml-mode yasnippet))
+    yaml-mode yasnippet bbdb))
 
 (dolist (p packages-list)
   (unless (or (member p package-activated-list)
